f6e88eb130afa1340efb1989f91aac0d1e1a0e91
efford.wang <efford.wang@applovin.com>
2023-08-23 20:37:14 +0800

    update logo and icon

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -17,7 +17,7 @@ const Settings: ProLayoutProps & {
   colorWeak: false,
   title: 'XUANWU',
   pwa: true,
-  logo: '/logo.svg',
+  logo: '/logo/logo.png',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
